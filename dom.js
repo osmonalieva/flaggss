35acb4759f4232698792cd56678650f82202c935
osmonalieva <nosmonalieva69@gmail.com>
2024-01-20 11:00:50 +0600

    first commit

---
+++ dom.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let btn = document.getElementById("btn")
+  let flag = document.getElementById("flag")
+  fetch("https://restcountries.com/v3.1/all")
+   .then(response => response.json())
+   .then(data => {
+    btn.addEventListener("click", function () {
+     let div = document.createElement("div")
+     div.classList.add("flagd")
+     let currentCountry = data[Math.floor(Math.random() * data.length)]
+     div.innerHTML = `
+           <img src="${currentCountry.flags.png}" alt="Flag">
+           <h2>${currentCountry.name.common}</h2>
+         `
+     flag.append(div)
+    })
+   })
+ })
+ 
+   
